20ea239608f90ca724e128befaf3d4d24a123cfd
sedx876 <swatkins876@gmail.com>
2020-11-28 08:22:40 -0500

    code cleanup

--- src/components/Pie.js
+++ src/components/Pie.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
 import './Pie.css'
+import Slice from './Slices/Slices';
+import Questions from './Questions/Questions';
 
 class Pie extends Component {
 
   state= {
+    slices: {
     1: {
       question: 'Health',
       rotate: 'rotate(0.0000, 220, 220)',
@@ -53,18 +56,53 @@ class Pie extends Component {
       transform: '0.1'
     },
   }
+}
+
+  selectScore = question => e => {
+    const targetValue = e.target.value === '10' ? '1' : '0.' + e.target.value
+    const newState = Object.assign({}, this.state);
+    newState.slices[question].transform = targetValue;
+    this.setState(newState);
+}
 
   render() {
     return (
       <div className="Content">
         <div className="QuestionBlock">
           <ul>
-            <li>Question</li>
+          <Questions
+            slices={this.state.slices}
+            selectScore={this.selectScore}
+          />
           </ul>
         </div>
       <div className="ChartBlock">
         <div className="ChartPie" style={{width: '450px'}}>
-          Chart here
+        <svg className="ChartPieChunk" width="440px" height="440px" viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <g id="overlay">
+            <rect x="219" y="20" height="200" width="2" fill="#FFF"></rect>
+            <g transform="translate(210, 10)">
+              <path fill="#000"></path>
+            </g>
+          </g>
+      </defs>
+      <mask id="wedge-mask" fill="white">
+        <path transform="translate(20, 20)" d="M93 7.241a200.006 200.006 0 01173.551-.865L200.004 200 112.33 20.241z" fillRule="nonzero"></path>
+      </mask>
+      <circle cx="220" cy="220" r="200" fill="#EFEFEF" stroke="#E3E3E3" strokeWidth="1"></circle>
+        <Slice
+          slices={this.state.slices}
+        />
+        <use xlinkHref="#overlay" transform="rotate(19, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(63.7143, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(108.7143, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(153.7143, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(198.7143, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(243.7143, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(288.7143, 220, 220)"></use>
+        <use xlinkHref="#overlay" transform="rotate(333.7143, 220, 220)"></use>
+        </svg>
         </div>
       </div>
       </div>
